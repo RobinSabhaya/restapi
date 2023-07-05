c495a9a3bf36da9b63af645a562a80f494c18cec
Robin Sabhaya <robin47ppsv@gmail.com>
2023-07-05 10:37:05 +0530

    deleted file

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://RobinSabhaya:Robin_11117@cluster0.jiwrx8s.mongodb.net/restApi?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true,useUnifiedTopology : true}).then(() => {
+const MONGODB_URL = process.env.MONGODB_URL
+mongoose.connect(MONGODB_URL, {useNewUrlParser: true, useUnifiedTopology: true,useUnifiedTopology : true}).then(() => {
     console.log('Connection Successfull');
 }).catch((err) => {
     console.log(err);
